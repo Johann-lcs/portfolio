5caf3510838e207107d500ebc6b885cb1c92c840
Johann <johannlucas132@gmail.com>
2026-02-25 10:26:53 +0100

    add all files

---
+++ script.js
@@ -0,0 +1,19 @@
+const track = document.querySelector('.carousel-track');
+    const slides = document.querySelectorAll('.skill-slide');
+    const nextBtn = document.querySelector('.next');
+    const prevBtn = document.querySelector('.prev');
+
+    let currentIndex = 0;
+    const slideWidth = slides[0].offsetWidth + 40; // largeur + gap
+
+    nextBtn.addEventListener('click', () => {
+        currentIndex++;
+        track.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+    });
+
+    prevBtn.addEventListener('click', () => {
+        if (currentIndex > 0) {
+            currentIndex--;
+            track.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+        }
+    });
